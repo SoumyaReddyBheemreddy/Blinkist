d93341a87868ab907ef09a34fabb560f8c9a5e51
SoumyaReddyBheemreddy <soumyareddy.bheemreddy@zemosolabs.com>
2022-01-28 16:35:31 +0530

    updated avatar test

--- src/components/atoms/avatar/AvatarIcon.test.tsx
+++ src/components/atoms/avatar/AvatarIcon.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import AvatarIcon from "./AvatarIcon";
+
 test('renders a avatarw', () => {
   render(<AvatarIcon children='A'/>);
   const avatarElement = screen.getByText(/A/i);

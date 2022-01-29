d015e0046ec42b6991932b5e41b3a3b21667ab7e
SoumyaReddyBheemreddy <soumyareddy.bheemreddy@zemosolabs.com>
2022-01-29 10:36:53 +0530

    molecules testcases

--- src/components/molecule/tab/ListTabs.test.tsx
+++ src/components/molecule/tab/ListTabs.test.tsx
@@ -3,7 +3,9 @@ import {  fireEvent, render, screen } from "@testing-library/react";
 import ListTabs from "./ListTabs";
 
 test("render a tab", () => {
-  render(<ListTabs />);
+  render(<ListTabs stateHandler={(value)=>{
+      console.log("changing state of parent tab state");
+  }}/>);
   const currentlyReading = screen.getByRole("tab",{name:/Currently reading/i});
   const finishedReading = screen.getByRole("tab",{name:/Finished/i})
   expect(currentlyReading).toHaveAttribute("aria-selected", "true");
